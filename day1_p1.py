9e3f8a2b343f99702c89db23d2674f29a3c9e674
Yi Hu <yihucd@gmail.com>
2025-11-30 21:41:47 -0800

    day1 done

--- day1_p1.py
+++ day1_p1.py
@@ -31,3 +31,40 @@ def get_content(filename):
     except Exception as e:
         print(f'file reading error. See details {e}')
 
+def get_steps(lines):
+    """Get the steps for unlocking the safe.
+    
+    Args:
+        lines (list): Input lines from the text file.
+
+    Returns:
+        A list of steps each of which is a tuple, e.g., ('L', 30).
+    """
+    steps = []
+    for line in lines:
+        direction = line[0]
+        counts = int(line[1:])
+        steps.append((direction, counts))
+    
+    return steps
+
+def get_num_zeros(steps, start_num):
+    """Calculate the number of times reaching zero."""
+
+    zero_count = 0
+    curr_num = start_num
+    for direction, step in steps:
+        if direction == 'L':
+            curr_num = (curr_num - step) % 100     
+        else:
+            curr_num = (curr_num + step) % 100
+        
+        if curr_num == 0:
+            zero_count += 1
+    
+    return zero_count
+
+lines = get_lines('day1.txt')
+steps = get_steps(lines)
+num_zeros = get_num_zeros(steps, 50)
+print(num_zeros)
---
+++ day1_p2.py
@@ -0,0 +1,72 @@
+import os
+
+demo_mode = int(os.getenv('demo'))
+
+def get_lines(filename):
+    demo_filename = 'demo.txt'
+    try:
+        if not demo_mode:
+            with open(filename, 'r') as file:
+                lines = [line.strip() for line in file]
+        else:
+            with open(demo_filename, 'r') as file:
+                lines = [line.strip() for line in file]
+        
+        return lines
+    except Exception as e:
+        print(f'File read operation exception happened. See details: {e}')
+        return []
+    
+def get_content(filename):
+    demo_filename = 'demo.txt'
+
+    try:
+        if not demo_mode:
+            with open(filename, 'r') as file:
+                content = file.read()
+        else:
+            with open(demo_filename, 'r') as file:
+                content = file.read()        
+        return content
+    except Exception as e:
+        print(f'file reading error. See details {e}')
+
+def get_steps(lines):
+    """Get the steps for unlocking the safe.
+    
+    Args:
+        lines (list): Input lines from the text file.
+
+    Returns:
+        A list of steps each of which is a tuple, e.g., ('L', 30).
+    """
+    steps = []
+    for line in lines:
+        direction = line[0]
+        counts = int(line[1:])
+        steps.append((direction, counts))
+    
+    return steps
+
+def get_num_zeros(steps, start_num):
+    """Calculate the number of times reaching zero."""
+
+    zero_count = 0
+    curr_num = start_num
+    for direction, step in steps:
+        if direction == 'L':
+            if  curr_num <= step % 100:
+                zero_count += 1
+                print(f'{direction}, {step}')
+        else: # 'R'
+            if curr_num + step % 100 >= 100:
+                zero_count += 1
+                print(f'{direction}, {step}')
+
+    
+    return zero_count
+
+lines = get_lines('day1.txt')
+steps = get_steps(lines)
+num_zeros = get_num_zeros(steps, 50)
+print(num_zeros)
